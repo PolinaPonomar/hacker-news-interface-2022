378af9b23bf3df3f10530341cae10de103765e6d
Polina Ponomareva <pp09117@gmail.com>
2022-10-25 20:23:38 +0300

    try to publised

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import {store} from "../src/store/index"
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <Provider store={store}>
       <App/>
     </Provider>
